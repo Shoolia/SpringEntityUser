77c264ff04fc2ee064ac9c918c36d0e1795009df
Dima Shoolia <mrschulpov97@gmail.com>
2019-12-16 10:47:35 +0200

    1 commit. Create Entity User + create for it applicationContext.xml - for String framework.

---
+++ src/main/java/ua/training/model/entity/User.java
@@ -0,0 +1,47 @@
+package ua.training.model.entity;
+
+public class User {
+    private String name;
+    private String surname;
+    private int age;
+    private String nickname;
+
+    public User(String name, String surname, int age, String nickname) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
