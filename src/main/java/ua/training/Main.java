c5af316bd37ed79687e9d8f3165c5b610c71690c
Dima Shoolia <mrschulpov97@gmail.com>
2019-12-16 10:57:28 +0200

    2 commit. add Main

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,7 +1,18 @@
 package ua.training;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.training.model.entity.User;
+
 public class Main {
     public static void main(String[] args) {
 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        User user = context.getBean("user",User.class);
+
+        System.out.println("Information about user: \n" + "Name: " +  user.getName() + "\n" +"Surname: " +  user.getSurname() +
+                "\n" + "Age:" + user.getAge() + "\n" + "Nickname: " +  user.getNickname());
+
+        context.close();
     }
 }
